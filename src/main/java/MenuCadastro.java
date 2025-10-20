347064b3bb94b7cec8ded203598182c794891576
MIGUEL RICARDO SEBASTIANA <miguel_sebastiana@estudante.sesisenai.org.br>
2025-10-20 20:53:29 -0300

    Chore: Adiciona o MenuLogin e MenuCadastro

---
+++ src/main/java/MenuCadastro.java
@@ -0,0 +1,31 @@
+public class MenuCadastro
+{
+    public static void Menu(){
+
+
+        Ferramentas.limpaTerminal();
+
+        System.out.println("      ==================");
+        System.out.println("      ==   Cadastro   ==");
+        System.out.println("      ==================");
+
+
+
+        System.out.println("\n\n\n\n------------------");
+        System.out.print("-Digite o seu cpf: ");
+        String cpf = Ferramentas.lString();
+        System.out.println("------------------");
+
+        System.out.println("\n\n------------------");
+        System.out.print("-Digite a sua senha: ");
+        String senha = Ferramentas.lString();
+        System.out.println("------------------");
+
+
+
+    }
+
+
+}
+
+}
--- src/main/java/MenuLogin.java
+++ src/main/java/MenuLogin.java
@@ -1,15 +1,30 @@
+import java.util.InputMismatchException;
+
+
 public class MenuLogin {
 
 
     public static void Menu(){
 
-        boolean continuar = true;
 
-        do {
+        Ferramentas.limpaTerminal();
+
+        System.out.println("      =================");
+        System.out.println("      ===   Login   ===");
+        System.out.println("      =================");
+
+
+        System.out.println("\n\n\n------------------");
+        System.out.print("-Digite o seu cpf: ");
+        String cpf = Ferramentas.lString();
+
+        System.out.print("\n\n-Digite a sua senha: ");
+        String senha = Ferramentas.lString();
+        System.out.println("------------------");
 
 
 
-
-        }while (!continuar);
     }
+
+
 }
