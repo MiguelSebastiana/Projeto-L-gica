d607d6fe8304033ef4c4ecbc0b92289e3f5c1ae4
MIGUEL RICARDO SEBASTIANA <miguel_sebastiana@estudante.sesisenai.org.br>
2025-10-20 21:44:12 -0300

    Chore: Adiciona Classe Gerente e Classe Super Visor

--- src/main/java/ClasseGerente.java
+++ src/main/java/ClasseGerente.java
@@ -1,2 +1,21 @@
-public class ClasseGerente {
+import java.util.Date;
+
+public class ClasseGerente extends ClasseUsuario{
+
+    private int tempo_na_funcao_anos_gerente;
+
+    public ClasseGerente(int id, String nome, String cpf, int nivelacesso, String telefone, double salario, Date datanasci, String email, int cargahoraria, String formacao, ClasseSetor setor, int tempo_na_funcao_anos_gerente){
+        super(id,nome,cpf,nivelacesso,telefone,salario,datanasci,email,cargahoraria,formacao,setor);
+        this.tempo_na_funcao_anos_gerente = tempo_na_funcao_anos_gerente;
+    }
+
+
+
+    public int getTempo_na_funcao_anos_gerente() {
+        return tempo_na_funcao_anos_gerente;
+    }
+
+    public void setTempo_na_funcao_anos_gerente(int tempo_na_funcao_anos_gerente) {
+        this.tempo_na_funcao_anos_gerente = tempo_na_funcao_anos_gerente;
+    }
 }
