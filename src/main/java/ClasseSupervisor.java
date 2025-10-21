3b7f5c4a15570fc0b7a2fe8a9ea5280f1dba4f27
eduardo_silva173 <gefferteduardo@gmail.com>
2025-10-20 21:14:46 -0300

    Chore: Adiciona Classes

---
+++ src/main/java/ClasseSupervisor.java
@@ -0,0 +1,2 @@
+public class ClasseSupervisor {
+}
--- src/main/java/ClasseUsuario.java
+++ src/main/java/ClasseUsuario.java
@@ -1,2 +1,23 @@
+import java.util.Date;
+
 public class ClasseUsuario {
+
+
+    //Atributos
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private int nivelacesso;
+    private String telefone;
+    private double salario;
+    private Date datanasci;
+    private String email;
+    private int cargahoraria;
+    private String formacao;
+    private ClasseSetor setor;
+    private
+
+
+
 }
