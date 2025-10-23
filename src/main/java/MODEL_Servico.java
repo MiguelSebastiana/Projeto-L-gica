01930ddbcf3e28e295717de93556bb8ed10eca8f
RENATO DE MEDEIROS <renato_de-medeiros@estudante.sesisenai.org.br>
2025-10-23 16:00:25 -0300

    Chore: Adiciona Model Ordem de serviço

--- src/main/java/MODEL_Servico.java
+++ src/main/java/MODEL_Servico.java
@@ -1,2 +1,57 @@
 public class MODEL_Servico {
+
+        private int ordem_servico;
+        private String status_aberto_ordem_servico;
+        private String descricao_ordem_servico;
+        private MODEL_Tecnico tecnico;
+        private MODEL_Maquina maquina;
+
+    public MODEL_Servico(int ordem_servico, String status_aberto_ordem_servico, String descricao_ordem_servico, MODEL_Tecnico tecnico, MODEL_Maquina maquina) {
+
+        this.ordem_servico = ordem_servico;
+        this.status_aberto_ordem_servico = status_aberto_ordem_servico;
+        this.descricao_ordem_servico = descricao_ordem_servico;
+        this.tecnico = tecnico;
+        this.maquina = maquina;
+    }
+
+    public int getOrdem_servico() {
+        return ordem_servico;
+    }
+
+    public void setOrdem_servico(int ordem_servico) {
+        this.ordem_servico = ordem_servico;
+    }
+
+    public String getStatus_aberto_ordem_servico() {
+        return status_aberto_ordem_servico;
+    }
+
+    public void setStatus_aberto_ordem_servico(String status_aberto_ordem_servico) {
+        this.status_aberto_ordem_servico = status_aberto_ordem_servico;
+    }
+
+    public String getDescricao_ordem_servico() {
+        return descricao_ordem_servico;
+    }
+
+    public void setDescricao_ordem_servico(String descricao_ordem_servico) {
+        this.descricao_ordem_servico = descricao_ordem_servico;
+    }
+
+    public MODEL_Tecnico getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(MODEL_Tecnico tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public MODEL_Maquina getMaquina() {
+        return maquina;
+    }
+
+    public void setMaquina(MODEL_Maquina maquina) {
+        this.maquina = maquina;
+    }
 }
