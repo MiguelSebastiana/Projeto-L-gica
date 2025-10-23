a8cce221d102639490605d71bcc7dea5cc290eb6
GUILHERME GONCALVES CHERPINSKI <guilherme_cherpin@estudante.sesisenai.org.br>
2025-10-23 19:45:19 -0300

    Fit: Ajuste no validator de setor

--- src/main/java/Validator_Setor.java
+++ src/main/java/Validator_Setor.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Validator_Setor {
 
     // ---- LISTA DE SETORES DISPONIVEIS ---- //
-    public static final ArrayList<String> setoresValidos = new Arrays.asList(
+    public static final List<String> setoresValidos = Arrays.asList(
             "Manutenção",
             "Produção",
             "TI",
@@ -14,7 +15,6 @@ public class Validator_Setor {
             "Montagem",
             "Controle de Qualidade"
     );
-
 
     // ---- VALIDAÇÃO DO SETOR ---- //
     public static String validarSetor(String nome){
