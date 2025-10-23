9cb84b24fc8c3f44351e8dde6b506bed68aee0db
MIGUEL RICARDO SEBASTIANA <miguel_sebastiana@estudante.sesisenai.org.br>
2025-10-23 15:10:45 -0300

    Chore: Adiciona o esquelo dos menus

--- src/main/java/MenuSuperVisor.java
+++ src/main/java/MenuSuperVisor.java
@@ -1,2 +1,43 @@
-public class MenuSupervisor {
+import java.util.InputMismatchException;
+
+public class MenuSuperVisor {
+
+    public static void Menu(){
+
+        boolean continuar = true;
+
+        do{
+            System.out.println("      =====================");
+            System.out.println("      == Menu SuperVisor ==");
+            System.out.println("      =====================");
+
+            System.out.println("1 - Ver status de todos OS");
+            System.out.println("2 - Criar ordens de serviço");
+            System.out.println("3 - Sair");
+
+            int escolha = 0;
+
+            try {
+                escolha = Ferramentas.lInteiro();
+            } catch (InputMismatchException e) {
+                System.err.println("VALOR DIGITADO  INVALIDO!");
+            }
+
+            switch (escolha) {
+                case 1:{
+                }
+
+                case 3:{
+                    MenuInicial.Menu();
+                }
+            }
+
+        }while (!continuar);
+
+
+    }
+
+
+
+
 }
--- src/main/java/MenuTecnico.java
+++ src/main/java/MenuTecnico.java
@@ -1,2 +1,43 @@
+import java.util.InputMismatchException;
+
 public class MenuTecnico {
+
+    public static void Menu(){
+
+        boolean continuar = true;
+
+        do{
+            System.out.println("      ====================");
+            System.out.println("      ==  Menu Tecnico  ==");
+            System.out.println("      ====================");
+
+            System.out.println("1 - Ver as ordens atribuidas");
+            System.out.println("2 - Alterar os Status das ordens");
+            System.out.println("3 - Sair");
+
+            int escolha = 0;
+
+            try {
+                escolha = Ferramentas.lInteiro();
+            } catch (InputMismatchException e) {
+                System.err.println("VALOR DIGITADO  INVALIDO!");
+            }
+
+            switch (escolha) {
+                case 1:{
+                }
+
+                case 3:{
+                    MenuInicial.Menu();
+                }
+            }
+
+        }while (!continuar);
+
+
+    }
+
+
+
+
 }
