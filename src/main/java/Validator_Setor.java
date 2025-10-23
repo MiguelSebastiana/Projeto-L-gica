ae03339e5571d7ff904b34e1f5570827da4afe17
GUILHERME GONCALVES CHERPINSKI <guilherme_cherpin@estudante.sesisenai.org.br>
2025-10-23 19:42:18 -0300

    Fit: AdiCrAjustes na model e  ajustes de validação

---
+++ src/main/java/Validator_Setor.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Validator_Setor {
+
+    // ---- LISTA DE SETORES DISPONIVEIS ---- //
+    public static final ArrayList<String> setoresValidos = new Arrays.asList(
+            "Manutenção",
+            "Produção",
+            "TI",
+            "Fundição",
+            "Usinagem",
+            "Bobinagem",
+            "Montagem",
+            "Controle de Qualidade"
+    );
+
+
+    // ---- VALIDAÇÃO DO SETOR ---- //
+    public static String validarSetor(String nome){
+        if (nome == null || nome.trim().isEmpty()){
+            throw new IllegalArgumentException("ERRO - NOME DE SETOR NÃO PODE SER VAZIO!");
+        }
+
+        String setor = nome.trim();
+
+        if (!setoresValidos.contains(setor)){
+            throw new IllegalArgumentException("ERRO - SETOR INVÁLIDO!");
+        }
+
+        return setor;
+    }
+}
