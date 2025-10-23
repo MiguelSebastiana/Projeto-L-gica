a82a1b262dc1cf6b90dc12e1bbf7ef6069272f3a
GUILHERME GONCALVES CHERPINSKI <guilherme_cherpin@estudante.sesisenai.org.br>
2025-10-23 20:02:38 -0300

    Fit: Ajustes na validaçaão de setores

--- src/main/java/Validator_Setor.java
+++ src/main/java/Validator_Setor.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Validator_Setor {
 
-    // ---- LISTA DE SETORES DISPONIVEIS ---- //
-    public static final List<String> setoresValidos = Arrays.asList(
+    // ---- LISTA DE SETORES DISPONÍVEIS POR STRING ----
+    public static final ArrayList<String> setoresValidos = new ArrayList<>(Arrays.asList(
             "Manutenção",
             "Produção",
             "TI",
@@ -14,17 +13,17 @@ public class Validator_Setor {
             "Bobinagem",
             "Montagem",
             "Controle de Qualidade"
-    );
+    ));
 
-    // ---- VALIDAÇÃO DO SETOR ---- //
-    public static String validarSetor(String nome){
-        if (nome == null || nome.trim().isEmpty()){
+    // ---- VALIDAÇÃO DO SETOR ----
+    public static String validarSetor(String nome) {
+        if (nome == null || nome.trim().isEmpty()) {
             throw new IllegalArgumentException("ERRO - NOME DE SETOR NÃO PODE SER VAZIO!");
         }
 
         String setor = nome.trim();
 
-        if (!setoresValidos.contains(setor)){
+        if (!setoresValidos.contains(setor)) {
             throw new IllegalArgumentException("ERRO - SETOR INVÁLIDO!");
         }
 
