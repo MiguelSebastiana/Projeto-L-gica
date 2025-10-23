39da160315525d5ab973d11cc27089ecfe070588
GUILHERME GONCALVES CHERPINSKI <guilherme_cherpin@estudante.sesisenai.org.br>
2025-10-23 16:37:55 -0300

    Fit: Término das validações de usúario, técnico, supervisor e gerente

--- src/main/java/Validator_Gerente.java
+++ src/main/java/Validator_Gerente.java
@@ -1,2 +1,10 @@
-public class Validator_Gerente {
+public class Validator_Gerente{
+
+    public static int validarTempoFuncao(int tempo){
+        if (tempo <= 10){
+            throw new IllegalArgumentException("ERRO - GERENTE DEVE TER NO MÍNIMO 10 ANOS DE FUNÇÃ0!");
+        }
+
+        return tempo;
+    }
 }
--- src/main/java/Validator_Tecnico.java
+++ src/main/java/Validator_Tecnico.java
@@ -1,2 +1,11 @@
 public class Validator_Tecnico {
+
+    // ---- VALIDAÇÃO DE ESPECIALIDADE DO TECNICO ---- //
+    public static String validarEspecialidade(String especialidade){
+        if (especialidade == null || especialidade.trim().isEmpty()){
+            throw new IllegalArgumentException("ERRO - ESPECIALIDADE NÃO PODE SER VAZIA!");
+        }
+
+        return especialidade.trim();
+    }
 }
