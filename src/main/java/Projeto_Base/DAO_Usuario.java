4cf9e526585b7dd48494422769ea0839da248c35
GABRIEL BEHLING <gabriel_behling@estudante.sesisenai.org.br>
2025-10-27 16:55:28 -0300

    Fix: arruma DAO_usuario

--- src/main/java/Projeto_Base/DAO_Usuario.java
+++ src/main/java/Projeto_Base/DAO_Usuario.java
@@ -1,22 +1,99 @@
 package Projeto_Base;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class DAO_Usuario
 {
 
-    public ArrayList<MODEL_Usuario> findAllUsers()
+    public static ArrayList<MODEL_Usuario> findAllUsers()
     {
 
         ArrayList<MODEL_Usuario> listaUsuarios = new ArrayList<>();
 
-        String querySql = "SELECT * FROM Usuario";
-//
-        //try {
-        //    ConnectionFactory conexao =
-        //}
+        String querySql = "select u.id_usuario, u.nome_usuario, u.cpf_usuario, u.nivel_acesso_usuario,\n" +
+                "u.telefone_usuario, u.salario_usuario,u.data_nasc_usuario,u.email_usuario, \n" +
+                "u.carga_horaria_minutos_usuario,u.formacao_usuario,\n" +
+                " u.Setor_id_setor, g.tempo_na_funcao_anos_gerente,sp.experiencia_anos_supervisor, \n" +
+                " t.especialidade_tecnico,t.status_disponibilidade_tecnico\n" +
+                "from Usuario u\n" +
+                "left join Gerente g on u.id_usuario = g.Usuario_id_usuario\n" +
+                "left join Supervisor sp on u.id_usuario = sp.Usuario_id_usuario \n" +
+                "left join Tecnico t on u.id_usuario = t.Usuario_id_usuario\n" +
+                "order by u.id_usuario;";
 
-return null;
+        try (Connection conexao = ConnectionFactory.getConn();
+             PreparedStatement stmt = conexao.prepareStatement(querySql);
+             ResultSet resultSet = stmt.executeQuery())
+        {
+            int idUsuario = resultSet.getInt("id_usuario");
+            String nomeUsuario = resultSet.getString("nome_usuario");
+            String cpfUsuario = resultSet.getString("cpf_usuario");
+            int nivelAcesso = resultSet.getInt("nivel_acesso_usuario");
+            String telefoneUsuario = resultSet.getString("telefone_usuario");
+            Double salarioUsuario = resultSet.getDouble("salario_usuario");
+            Date dataNascimento = resultSet.getDate("data_nasc_usuario");
+            String emailUsuario = resultSet.getString("email_usuario");
+            int cargaHoraria = resultSet.getInt("carga_horaria_minutos_usuario");
+            String formacaoUsuario = resultSet.getString("formacao_usuario");
+            int idSetor = resultSet.getInt("Setor_id_setor");
+
+
+            if(nivelAcesso == 1)
+            {
+                String especialidadeTecnico = resultSet.getString("especialidade_tecnico");
+                boolean status;
+
+                if(resultSet.getString("status_disponibilidade_tecnico").equals("Disponível"))
+                {
+                    status = true;
+                }
+                else
+                {
+                    status = false;
+                }
+
+                MODEL_Usuario tecnico = new MODEL_Tecnico(idUsuario, nomeUsuario, cpfUsuario, nivelAcesso, telefoneUsuario,salarioUsuario,
+                        dataNascimento, emailUsuario, cargaHoraria, formacaoUsuario,idSetor,especialidadeTecnico,status);
+
+                listaUsuarios.add(tecnico);
+            }
+            else if( nivelAcesso == 2)
+            {
+                int anosSupervisor = resultSet.getInt("experiencia_anos_supervisor");
+
+                MODEL_Usuario supervisor = new MODEL_Supervisor(idUsuario, nomeUsuario, cpfUsuario, nivelAcesso, telefoneUsuario,salarioUsuario,
+                        dataNascimento, emailUsuario, cargaHoraria, formacaoUsuario,idSetor,anosSupervisor);
+
+                listaUsuarios.add(supervisor);
+            }
+            else if(nivelAcesso == 3)
+            {
+                int anosGerente = resultSet.getInt("tempo_na_funcao_anos_gerente");
+
+                MODEL_Usuario gerente = new MODEL_Gerente(idUsuario, nomeUsuario, cpfUsuario, nivelAcesso, telefoneUsuario,salarioUsuario,
+                        dataNascimento, emailUsuario, cargaHoraria, formacaoUsuario,idSetor,anosGerente);
+
+                listaUsuarios.add(gerente);
+            }
+
+            return listaUsuarios;
+        }
+        catch (SQLException e)
+        {
+
+            System.err.println("Não foi possível buscar todos os usuários: " + e.getMessage());
+
+            throw new RuntimeException("Erro ao consultar o banco de dados.", e);
+        }
+
     }
 
+    public static void main(String[] args) {
+        System.out.println(findAllUsers());
+    }
 }
