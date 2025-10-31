09528638907f7d2b70005e57c8fd409386e6dd29
GUILHERME GONCALVES CHERPINSKI <guilherme_cherpin@estudante.sesisenai.org.br>
2025-10-30 21:34:39 -0300

    Fit: Criação de exceções personalizadas!

--- src/main/java/Exception/TEMPOFUNException.java
+++ src/main/java/Exception/TEMPOFUNException.java
@@ -1,7 +1,16 @@
 package Exception;
 
 public class TEMPOFUNException extends RuntimeException {
-  public TEMPOFUNException(String message) {
-    super(message);
-  }
+    public TEMPOFUNException(String message) {
+        super(message);
+    }
+
+    // -- MENSAGEM PADRAO -- //
+    public TEMPOFUNException(){
+      super("ERRO - TEMPO INVÁLIDO!");
+    }
+
+    public static TEMPOFUNException inferior(){
+      return new TEMPOFUNException("ERRO - GERENTE DEVE TER NO MÍNIMO 10 ANOS DE FUNÇÃO!");
+    }
 }
--- src/main/java/Util/Validator_Gerente.java
+++ src/main/java/Util/Validator_Gerente.java
@@ -1,10 +1,10 @@
 package Util;
-
+import Exception.*;
 public class Validator_Gerente{
 
     public static int validarTempoFuncao(int tempo){
         if (tempo <= 10){
-            throw new IllegalArgumentException("ERRO - GERENTE DEVE TER NO MÍNIMO 10 ANOS DE FUNÇÃ0!");
+            throw TEMPOFUNException.inferior();
         }
 
         return tempo;
