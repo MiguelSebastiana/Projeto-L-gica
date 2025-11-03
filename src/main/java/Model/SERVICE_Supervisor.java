9962e94f4e7bd14d44ad6f28281af18c8fee82a6
DIEGO WILLIAN WEEGE <diego_weege@estudante.sesisenai.org.br>
2025-11-03 20:18:16 -0300

    Fit: Adiciona metodos as services relacionadas aos usuarios

--- src/main/java/Model/SERVICE_Supervisor.java
+++ src/main/java/Model/SERVICE_Supervisor.java
@@ -1,41 +1,90 @@
 package Model;
 
 import java.util.ArrayList;
+import Repository.DAO_Supervisor;
+import Util.*;
+import Exception.ANOSEXPERIENCIAException;
 
 public class SERVICE_Supervisor {
 
     //--Atributos--//
-
+    static DAO_Supervisor daoSupervisor;
 
 
     //--Métodos--//
 
     //Create
 
-    public void Insert_Supervisor() {
-
+    public void Insert_Supervisor(MODEL_Supervisor supervisor)
+    {
+        try
+        {
+            if(supervisor != null)
+            {
+                daoSupervisor.insert_Supervisor(supervisor);
+            }
+        }
+        catch(RuntimeException re)
+        {
+            System.out.println(re.getMessage());
+        }
     }
 
     //Read
 
-    public ArrayList<MODEL_Supervisor> Find_All_Supervisores(){
+    public ArrayList<MODEL_Supervisor> Find_All_Supervisores()
+    {
+        try {
+            return daoSupervisor.find_All_Supervisores();
+        }catch(RuntimeException re)
+        {
+            System.out.println(re.getMessage());
+        }
 
+        return null;
     }
 
-    public MODEL_Supervisor Find_By_Id(){
+    public MODEL_Supervisor Find_By_Id(int id)
+    {
+        try
+        {
+            return daoSupervisor.find_By_Id(id);
+        }
+        catch(RuntimeException re)
+        {
+            System.out.println(re.getMessage());
+        }
 
+        return null;
     }
 
     //Update
 
-    public void Update_Experiencia(){
-
+    public void Update_Experiencia(MODEL_Supervisor supervisor, int experiencia)
+    {
+        try
+        {
+            daoSupervisor.update_Experiencia(supervisor, experiencia);
+        }catch(ANOSEXPERIENCIAException aee)
+        {
+            System.out.println(aee.getMessage());
+        }
     }
 
     //Delete
 
-    public void Delete_Supervisor(){
-
+    public void Delete_Supervisor(MODEL_Supervisor supervisor)
+    {
+        try
+        {
+            if(supervisor != null)
+            {
+                daoSupervisor.delete_Supervisor(supervisor);
+            }
+        }catch(RuntimeException re)
+        {
+            System.out.println(re.getMessage());
+        }
     }
 
     //Outros
